3f05df1ec8def4da191b9e1ed58da5c897b004d1
Mohsen Majidi <mohsen.majidi22@gmail.com>
2023-02-19 08:51:16 +0330

    simple Pinia store is added to the project

---
+++ pages/learning/pinia.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import {useCounterStore} from "~/store/useCounter";
+
+const counterStore = useCounterStore()
+
+</script>
+
+<template>
+    <h2>Welcome to Pinia in vue</h2>
+    <h3>information from store -> count : {{ counterStore.count }}</h3>
+    <button class="btn-primary" @click="counterStore.count ++">increment</button>
+    <button class="btn-primary" @click="counterStore.increment">increment</button>
+</template>
