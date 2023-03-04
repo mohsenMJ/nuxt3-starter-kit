f723bf9d3a34454628be5dacce848e541de2c5c1
Mohsen Majidi <mohsen.majidi22@gmail.com>
2023-03-05 00:23:53 +0330

    Update receved from backend - bugs are fixed and Page and tempalte section are working

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,17 @@
         <slot/>
     </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+    transition-property: opacity;
+    transition-timing-function: ease-in-out;
+    transition-duration: 500ms;
+}
+.page-enter,
+.page-leave-to {
+    opacity: 0;
+}
+
+</style>
